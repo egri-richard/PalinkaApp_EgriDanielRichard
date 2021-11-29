eb910d9b7495edc186e583dc9271f7d8bf3fb4b9
Erinad-2002 <Erinad.business@gmail.com>
2021-11-29 15:48:45 +0100

    init

---
+++ app/src/main/java/com/example/palinkaapp/DBHandler.kt
@@ -0,0 +1,43 @@
+package com.example.palinkaapp
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+
+val DATABASE_NAME = "MyDB.db"
+val TABLE_NAME = "palinkak"
+val COL_ID = "id"
+val COL_FOZO = "fozo"
+val COL_GYUMOLCS = "gyumolcs"
+val COL_ALKOHOL = "alkohol"
+
+class DBHandler(var context:Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        val createTable =   "CREATE TABLE " + TABLE_NAME + " (" +
+                            COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                            COL_FOZO + " VARCHAR(256)," +
+                            COL_GYUMOLCS + " VARCHAR(256)," +
+                            COL_ALKOHOL + " INTEGER;"
+        p0?.execSQL(createTable)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun insert(p:Palinka) {
+        var db = this.writableDatabase
+        var cv = ContentValues()
+        cv.put(COL_FOZO, p.fozo)
+        cv.put(COL_GYUMOLCS, p.gyumolcs)
+        cv.put(COL_ALKOHOL, p.alkohol)
+        var result = db.insert(TABLE_NAME, null, cv)
+        if (result == (-1).toLong()) {
+            Toast.makeText(context, "Sikertelen", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Sikeres", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/palinkaapp/Palinka.kt
@@ -0,0 +1,17 @@
+package com.example.palinkaapp
+
+class Palinka {
+    var id: Int = 0
+    var fozo: String = ""
+    var gyumolcs: String = ""
+    var alkohol: Int = 0
+
+    constructor(Id: Int, Fozo: String, Gyumolcs: String, Alkohol: Int) {
+        this.id = Id
+        this.fozo = Fozo
+        this.gyumolcs = Gyumolcs
+        this.alkohol = Alkohol
+    }
+
+    constructor()
+}
